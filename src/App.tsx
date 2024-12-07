2fc5f6cfddb64ece2c95798f1045bcf513b2d4e2
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2024-12-07 14:05:39 +0400

    add full project with few error connectet to svgs

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,49 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import User from "./components/User";
 
+export interface Information {
+  avatar_url: string | null;
+  bio: string | null;
+  blog: string | null;
+  company: null;
+  created_at: string | null;
+  email: string | null;
+  events_url: string | null;
+  followers: number | null;
+  followers_url: string | null;
+  following: number | null;
+  following_url: string | null;
+  gists_url: string | null;
+  gravatar_id: string | null;
+  hireable: string | null;
+  html_url: string | null;
+  id: number | null;
+  location: string | null;
+  login: string | null;
+  name: string | null;
+  node_id: string | null;
+  organizations_url: string | null;
+  public_gists: number | null;
+  public_repos: number | null;
+  received_events_url: string | null;
+  repos_url: string | null;
+  site_admin: boolean;
+  starred_url: string | null;
+  subscriptions_url: string | null;
+  twitter_username: null;
+  type: string | null;
+  updated_at: string | null;
+  url: string | null;
+  user_view_type: string | null;
+}
+
 function App() {
   const [theme, setTheme] = useState(true);
-  const [user, setUser] = useState("octocat");
+  const [user, setUser] = useState<string>("octocat");
+  const [data, setData] = useState<Information | null | undefined>(null);
+  const [error, setError] = useState(false);
 
-  const githubData = async (userName: string | null) => {
+  const githubData = async (userName: string | Information) => {
     if (!userName) return;
 
     try {
@@ -17,10 +55,12 @@ function App() {
         throw new Error("No results!");
       }
       const person = await response.json();
-      setUser(person);
-      return person;
+      setData(person);
+      setError(false);
     } catch (error) {
       console.log("Something went wrong.");
+      // setData(undefined);
+      setError(true);
     }
   };
 
@@ -30,24 +70,24 @@ function App() {
   };
 
   useEffect(() => {
-    githubData(user);
+    if (typeof user === "string") {
+      githubData(user);
+    }
   }, []);
 
-  console.log(handleUserInput);
-  console.log(user);
-
   return (
-    <div className={`${theme ? "bg-[#f6f8ff]" : "bg-[#141d2f]"}`}>
+    <div className={theme ? "bg-[#f6f8ff]" : "bg-[#141d2f]"}>
       <Header theme={theme} setTheme={setTheme} />
       <div className="flex flex-col items-center">
         <Search
           theme={theme}
           user={user}
-          setUser={setUser}
           handleUserInput={handleUserInput}
           githubData={githubData}
+          data={data}
+          error={error}
         />
-        <User theme={theme} user={user} />
+        <User theme={theme} user={data} />
       </div>
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,18 @@
+import { Information } from "../App";
+
 const Search: React.FC<{
   theme: boolean;
-  user: string;
-  setUser: React.Dispatch<React.SetStateAction<string>>;
+  error: boolean;
+  user: string | Information;
   handleUserInput: React.ChangeEventHandler<HTMLInputElement>;
-  githubData: Promise<any>;
-}> = ({ theme, user, setUser, handleUserInput, githubData }) => {
+  githubData: (userName: string | Information) => Promise<any>;
+  data: Information | null | undefined;
+}> = ({ theme, user, handleUserInput, githubData, data, error }) => {
   return (
     <div
       className={`${
         theme ? "bg-[#fefefe]" : "bg-[#1e2a47]"
-      } flex items-center mt-[36px] rounded-[15px]  shadow-custom pt-[6.5px] pr-[7px] pb-[7.5px] pl-[16px] w-[327.2px] max-w-[327.2px] tablet:py-[9.5px] tablet:pr-[10px] tablet:pl-[32px] tablet:w-[573px] tablet:max-w-[573px] desktop:w-[730px] desktop:max-w-[730px] `}
+      } cursor-pointer flex items-center mt-[36px] rounded-[15px]  shadow-custom pt-[6.5px] pr-[7px] pb-[7.5px] pl-[16px] w-[327.2px] max-w-[327.2px] tablet:py-[9.5px] tablet:pr-[10px] tablet:pl-[32px] tablet:w-[573px] tablet:max-w-[573px] desktop:w-[730px] desktop:max-w-[730px] `}
     >
       <img
         src="/icon-search.svg"
@@ -23,13 +26,20 @@ const Search: React.FC<{
         name="user"
         className={`${
           theme ? "bg-[#fefefe] text-[#4b6a9b]" : "bg-[#1e2a47] text-[#ffffff]"
-        } w-full ml-[9px] text-[13px] font-normal leading-[1.92] tracking-normal text-left tablet:text-[18px] tablet:leading-[1.39] tablet:ml-[24px]`}
+        } w-full ml-[9px] text-[13px] font-normal leading-[1.92] tracking-normal text-left  tablet:text-[18px] tablet:leading-[1.39] tablet:ml-[24px] desktop:w-[60%]`}
       />
+      {!error ? null : (
+        <p className="w-[100%] tablet:w-[35%] desktop:w-[17%] text-[12px] ml-[8px] font-bold leading-normal text-left text-[#f74646] right-[320px] desktop:text-[15px]">
+          No results
+        </p>
+      )}
       <button
         onClick={() => {
           githubData(user);
         }}
-        className="ml-[7px] rounded-[10px] bg-[#0079ff] py-[12.5px] pr-[14px] pl-[18px] text-[14px] font-bold leading-normal text-center text-[#ffffff] tablet:text-[16px] tablet:pt-[12.5px] tablet:pr-[23px] tablet:pb-[13.5PX] tablet:pl-[24px] tablet:ml-[0]"
+        className={`${data !== undefined ? "desktop:ml-[110px]" : ""} ${
+          error ? "desktop:ml-[0px]" : ""
+        } hover:bg-[#60abff] ml-[7px] rounded-[10px] bg-[#0079ff] py-[12.5px] pr-[14px] pl-[18px] text-[14px] font-bold leading-normal text-center text-[#ffffff] tablet:text-[16px] tablet:pt-[12.5px] tablet:pr-[23px] tablet:pb-[13.5PX] tablet:pl-[24px] tablet:ml-[10px]`}
       >
         Search
       </button>
